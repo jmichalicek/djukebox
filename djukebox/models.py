9827e2d8361e66436a199d3e01035df4ce30968d
Justin Michalicek <jmichalicek@gmail.com>
2012-10-24 17:33:35 -0400

    Added blueimp jquery ajax file uploader and now file uploads are handled via ajax

--- djukebox/models.py
+++ djukebox/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
 from django.db.models.signals import post_delete
 from django.conf import settings
