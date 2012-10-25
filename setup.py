58f9e55d2ee1d785a0cbce346e0987186d5e1658
Justin Michalicek <jmichalicek@gmail.com>
2012-10-25 11:53:19 -0400

    Bumped the version because of the major UI change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 package_data = ['static/css/*', 'static/img/*', 'static/js/*', 'static/js/templates/*', 'templates/djukebox/*']
 dependencies = ['pillow', 'django-celery==2.5.5', 'mutagen', 'simplejson', 'django-bootstrap-form', 'django-tastypie==0.9.11']
 setup(name = "djukebox",
-      version = "0.0.1",
+      version = "0.1.0",
       description = "A django HTML 5 audio player",
       author = "Justin Michalicek",
       author_email = "jmichalicek@gmail.com",
