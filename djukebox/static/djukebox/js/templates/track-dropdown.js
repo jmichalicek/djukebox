a5818bf05a6f594e0b5ac57b9f39f252c2910120
Justin Michalicek <jmichalicek@gmail.com>
2012-12-17 10:27:23 -0500

    Issue #9 - added drop down menus for track items which is where delete functionality will live, enabled dropdown on all track list. Added ability to play from that dropdown menu at the same time because I got sidetracked.

---
+++ djukebox/static/djukebox/js/templates/track-dropdown.js
@@ -0,0 +1,5 @@
+<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu">
+  <li><a class="playbutton">Play</a></li>
+  <li><a href="#">Edit</a></li>
+  <li><a class="deletebutton" url="{{ this.resource_uri }}">Delete</a></li>
+</ul>
