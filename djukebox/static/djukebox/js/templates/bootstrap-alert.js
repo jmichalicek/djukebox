90bbd8eb599623c6fc3a9066072dcea9962c94fb
Justin Michalicek <jmichalicek@gmail.com>
2012-12-28 13:55:02 -0500

    issue #9 Added alert popup when an error occurs deleting a track

---
+++ djukebox/static/djukebox/js/templates/bootstrap-alert.js
@@ -0,0 +1,5 @@
+<div class="alert alert-block {{alert.class}}">
+    <button class="close" data-dismiss="alert" type="button">&times;</button>
+    <h4>{{alert.header}}</p>
+    <p>{{alert.message}}</p>
+</div>
