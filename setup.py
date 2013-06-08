793517d50d55c629c484fe3e562a69b30d9f5fcb
Justin Michalicek <jmichalicek@gmail.com>
2013-06-08 17:40:05 -0400

    Issue #18 set correct tastypie version in setup.py and bumped djukebox version number

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 package_data = ['static/css/*', 'static/img/*', 'static/js/*', 'static/js/templates/*', 'templates/djukebox/*']
-dependencies = ['pillow', 'django-celery==2.5.5', 'mutagen', 'simplejson', 'django-bootstrap-form', 'django-tastypie==0.9.11']
+dependencies = ['pillow', 'django-celery==2.5.5', 'mutagen', 'simplejson', 'django-bootstrap-form', 'django-tastypie==0.9.16']
 setup(name = "djukebox",
-      version = "0.1.0",
+      version = "0.2.0",
       description = "A django HTML 5 audio player",
       author = "Justin Michalicek",
       author_email = "jmichalicek@gmail.com",
